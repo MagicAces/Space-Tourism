f3e2fb10cafa55144ab990dee6fe3820b312ee44
MagicAces <isaac.oniti14@gmail.com>
2022-11-21 14:05:40 +0000

    Add PORT

--- data.js
+++ data.js
@@ -8,6 +8,6 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname+"/index.html");
 })
 
-app.listen(3000, ()=> {
+app.listen(process.env.PORT || 3000, ()=> {
   console.log("Server on port 3000");
 })
